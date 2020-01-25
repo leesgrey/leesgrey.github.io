762375f3044d390a8de2128daa51486dd25834b6
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-01-25 05:27:03 -0500

    Fixed mountain parallax and z index layering

--- js/parallax.js
+++ js/parallax.js
@@ -1,18 +1,40 @@
 'use strict';
 
-let refPoint = document.querySelector('h1').getBoundingClientRect();
-console.log(refPoint)
+document.addEventListener('DOMContentLoaded', init, false);
+function init(){
+  console.log("initializing")
 
-document.querySelector('.outer-wrapper').addEventListener('scroll', function(e) {
-  refPoint = document.querySelector('h1').getBoundingClientRect();
-  var scrolled = refPoint.x;
+  const left_arrow = document.querySelector('#leftarrow');
   const foreground = document.querySelector('.foreground');
-  const background = document.querySelector('.background')
-  foreground.style.left= - (scrolled * 0.2) + 'px';
-  background.style.left= + (scrolled * 0.2) + 'px';
-})
+  const background = document.querySelector('.background');
+  const midground = document.querySelector('.midground');
 
-document.addEventListener('click', function(e) {
-  refPoint = document.querySelector('h1').getBoundingClientRect();
-  console.log(refPoint);
-})
+  const menuLinks = document.getElementsByClassName("menuLink");
+  console.log(menuLinks)
+
+  for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      let target = e.target.href;
+      document.querySelector('.outer-wrapper').animate({scrollLeft: 900}, 300)
+      console.log("dfsfa")
+    })
+  }
+
+
+  let refPoint = document.querySelector('h1').getBoundingClientRect();
+
+  document.querySelector('#name').addEventListener('click', function(e) {
+    console.log(foreground.style.left)
+  })
+
+  document.querySelector('.outer-wrapper').addEventListener('scroll', function(e) {
+    console.log("scrolling")
+    refPoint = document.querySelector('.wrapper').getBoundingClientRect();
+    var scrolled = refPoint.x;
+
+    foreground.style.left =+ (scrolled * 0.15) + 'px';
+    midground.style.left =+ (scrolled * 0.1) + 'px';
+    background.style.left =+ (scrolled * 0.05) + 'px';
+  })
+}
