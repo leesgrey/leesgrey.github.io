9099a3f5cc335f5f63b50d2408f30089087535c9
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-01-27 06:19:23 -0500

    Added fade in and out

--- js/parallax.js
+++ js/parallax.js
@@ -10,11 +10,17 @@ function init(){
   const left_arrow = document.querySelector('#leftarrow');
   const right_arrow = document.querySelector('#rightarrow');
 
+  const fades = document.getElementsByClassName('fades');
+  console.log(fades)
+
   const foreground = document.querySelector('.foreground');
   const background = document.querySelector('.background');
   const midground = document.querySelector('.midground');
 
-  /* scroll to menu page */
+  const name = document.querySelector("#about");
+
+  /*
+  /* scroll to menu page
   const menuLinks = document.getElementsByClassName("menuLink");
   for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener('click', function(e) {
@@ -24,35 +30,63 @@ function init(){
       console.log("dfsfa")
     })
   }
+  */
 
   /* arrow click */
   left_arrow.addEventListener('click', function(e) {
-    console.log('left');
     wrapper.scrollBy({
       left: -window.innerWidth / 2,
       behavior: 'smooth'
     })
   })
   right_arrow.addEventListener('click', function(e) {
-    console.log('right')
     wrapper.scrollBy({
       left: window.innerWidth / 2,
       behavior: 'smooth'
     });
   })
 
+  moon.addEventListener('click', function(e) {
+    wrapper.scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
+
   moonshadow.style.left = (moon.offsetLeft + moon.offsetWidth) + 'px';
-  console.log(moon.offsetLeft)
+
+  /* stop scroll setup timer */
+  var timer;
 
   /* parallel scroll */
   let refPoint = document.querySelector('h1').getBoundingClientRect();
+  
   document.querySelector('.outer-wrapper').addEventListener('scroll', function(e) {
     refPoint = document.querySelector('.wrapper').getBoundingClientRect();
     var scrolled = refPoint.x;
 
+    clearTimeout(timer);
+    timer = setTimeout(function(){
+      heehee()
+    }, 30);
+
     foreground.style.left = (scrolled * 0.15) + 'px';
     midground.style.left = (scrolled * 0.1) + 'px';
     background.style.left = (scrolled * 0.05) + 'px';
-    moonshadow.style.left = (moon.offsetLeft + moon.offsetWidth) + (scrolled * 0.05) + 'px'
+
+    moonshadow.style.left = (moon.offsetLeft + moon.offsetWidth) + (scrolled * 0.02) + 'px'
   })
+
+  function heehee(){
+    console.log("heehee")
+    for (var i = 0; i < fades.length; i++) {
+      if (Math.abs(fades[i].getBoundingClientRect().x - (window.innerWidth / 3)) < (window.innerWidth / 4)) {
+        fades[i].style.opacity = 1;
+      }
+      else {
+        fades[i].style.opacity = 0;
+      }
+    }
+  }
 }
+
