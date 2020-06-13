1d2fe9a4b368dcb41302747d68fe6855fd7cabb2
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-06-13 14:03:14 -0400

    Vertical update

--- js/parallax.js
+++ js/parallax.js
@@ -1,72 +1,14 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', init, false);
+
 function init(){
   const wrapper = document.querySelector('.outer-wrapper');
-
   const moon = document.querySelector('#moon');
-
-  const left_arrow = document.querySelector('#leftarrow');
-  const right_arrow = document.querySelector('#rightarrow');
-
   const fades = document.getElementsByClassName('fades');
-
+  const name = document.querySelector("#about");
   const foreground = document.querySelector('.foreground');
   const background = document.querySelector('.background');
   const midground = document.querySelector('.midground');
 
-  const name = document.querySelector("#about");
-
-  /* arrow click */
-  left_arrow.addEventListener('click', function(e) {
-    wrapper.scrollBy({
-      left: -window.innerWidth / 2,
-      behavior: 'smooth'
-    })
-  })
-  right_arrow.addEventListener('click', function(e) {
-    wrapper.scrollBy({
-      left: window.innerWidth / 2,
-      behavior: 'smooth'
-    });
-  })
-
-  moonshadow.addEventListener('click', function(e) {
-    wrapper.scrollTo({
-      left: 0,
-      behavior: 'smooth'
-    })
-  })
-
-  /* stop scroll setup timer */
-  var timer;
-
-  /* parallel scroll */
-  let refPoint = document.querySelector('h1').getBoundingClientRect();
-  
-  document.querySelector('.outer-wrapper').addEventListener('scroll', function(e) {
-    refPoint = document.querySelector('.wrapper').getBoundingClientRect();
-    var scrolled = refPoint.x;
-
-    clearTimeout(timer);
-    timer = setTimeout(function(){
-      heehee()
-    }, 80);
-
-    foreground.style.left = (scrolled * 0.15) + 'px';
-    midground.style.left = (scrolled * 0.1) + 'px';
-    background.style.left = (scrolled * 0.05) + 'px';
-  })
-
-  function heehee(){
-    console.log("heehee")
-    for (var i = 0; i < fades.length; i++) {
-      if (Math.abs(fades[i].getBoundingClientRect().x - (window.innerWidth / 3)) < (window.innerWidth / 4)) {
-        fades[i].style.opacity = 1;
-      }
-      else {
-        fades[i].style.opacity = 0.3;
-      }
-    }
-  }
 }
 
