21442117236db1950758be63d2cc5f114ffd9408
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-06-15 13:29:54 -0400

    Added cursor, fade on page change

--- js/parallax.js
+++ js/parallax.js
@@ -9,7 +9,7 @@ function init(){
   const left_arrow = document.querySelector('#leftarrow');
   const right_arrow = document.querySelector('#rightarrow');
 
-  const content = document.getElementsByClassName('fades');
+  const content = document.getElementsByClassName('content');
 
   const foreground = document.querySelector('.foreground');
   const background = document.querySelector('.background');
@@ -18,17 +18,23 @@ function init(){
   const name = document.querySelector("#about");
   const menu = document.querySelector("#menu");
 
+  const star = document.querySelector("#cursor");
+  console.log(star)
+
   /* keep track of section */
   let current = 0;
+  console.log(content)
 
   /* arrow click */
   left_arrow.addEventListener('click', function(e) {
+    content[current].style.opacity = 0;
     current--;
     ArrowUpdate();
     MountainUpdate();
   })
 
   right_arrow.addEventListener('click', function(e) {
+    content[current].style.opacity = 0;
     if (right_arrow.classList.contains('fadedIn')) {
       right_arrow.classList.remove('fadedIn');
     }
@@ -37,13 +43,19 @@ function init(){
     MountainUpdate();
   })
 
+  $(document).bind('mousemove', function(e){
+      $('#tail').css({
+         left:  e.pageX - 10,
+         top:   e.pageY - 10
+      });
+  });
 
   function ArrowUpdate() {
     if (current == 0) {
       left_arrow.classList.remove("active");
       menu.style.left = ''
     }
-    else if (current == 4) {
+    else if (current == 5) {
       right_arrow.classList.remove("active");
     }
     else {
@@ -67,12 +79,19 @@ function init(){
   }
 
   function MountainUpdate() {
+    console.log(current)
+    content[current].style.opacity = 1;
     wrapper.scrollBy({
       left: $(".content").eq(current).offset().left,
       behavior: 'smooth'
     })
     console.log($(".content").eq(0).offset().left)
   }
+
 }
 
-
+/*
+if(window.pageYOffset != 0)
+  alert("You are at the top of this window");
+}
+*/
