5ff7c361debfff53aa19a49ee818ed7e75e5676a
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2020-06-16 16:47:27 -0400

    Reformatted page tracking

--- js/parallax.js
+++ js/parallax.js
@@ -3,19 +3,18 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init(){
   /* element selection */
   const wrapper = document.querySelector('.outer-wrapper');
-
   const moon = document.querySelector('#moon');
 
   const left_arrow = document.querySelector('#leftarrow');
   const right_arrow = document.querySelector('#rightarrow');
 
   const content = document.getElementsByClassName('content');
+  const pages = $(".content");
 
   const foreground = document.querySelector('.foreground');
   const background = document.querySelector('.background');
   const midground = document.querySelector('.midground');
 
-  const name = document.querySelector("#about");
   const menu = document.querySelector("#menu");
 
   const star = document.querySelector("#cursor");
@@ -24,8 +23,11 @@ function init(){
   const projects = document.getElementsByClassName('projects');
   const art = document.getElementsByClassName('art');
   const contact = document.getElementsByClassName('contact');
+
+  // tracks page
   let current = 0;
 
+  ChangePage(0);
 
   moon.addEventListener('click', function(e) {
     wrapper.scrollTo({
@@ -33,81 +35,30 @@ function init(){
       left: 0,
       behavior: 'smooth'
     })
-    current = 0;
-    MountainUpdate();
-    ArrowUpdate();
+    ChangePage(0);
   })
+
+  // menu buttons
   Array.from(about).forEach(function(x){
-    x.addEventListener('click', function(e) {
-      wrapper.scrollBy({
-        top: $("#about").offset().top,
-        left: $("#about").offset().left,
-        behavior: 'smooth'
-      })
-      content[current].style.opacity = 0;
-      current = 1;
-      MountainUpdate()
-      ArrowUpdate()
-    })
+    x.addEventListener('click', function() { ChangePage(1); });
   })
 
   Array.from(projects).forEach(function(x){
-    x.addEventListener('click', function(e) {
-      wrapper.scrollBy({
-        top: $("#projects").offset().top,
-        left: $("#projects").offset().left,
-        behavior: 'smooth'
-      })
-      content[current].style.opacity = 0;
-      current = 2;
-      MountainUpdate()
-      ArrowUpdate()
-    })
+    x.addEventListener('click', function() { ChangePage(2); });
   })
+
   Array.from(art).forEach(function(x){
-    x.addEventListener('click', function(e) {
-      wrapper.scrollBy({
-        top: $("#art").offset().top,
-        left: $("#art").offset().left,
-        behavior: 'smooth'
-      })
-      content[current].style.opacity = 0;
-      current = 3;
-      MountainUpdate()
-      ArrowUpdate()
-    })
+    x.addEventListener('click', function() { ChangePage(3); });
   })
+
   Array.from(contact).forEach(function(x){
-    x.addEventListener('click', function(e) {
-      wrapper.scrollBy({
-        top: $("#contact").offset().top,
-        left: $("#contact").offset().left,
-        behavior: 'smooth'
-      })
-      content[current].style.opacity = 0;
-      current = 4;
-      MountainUpdate()
-      ArrowUpdate()
-    })
+    x.addEventListener('click', function() { ChangePage(4); });
   })
 
   /* arrow click */
-  left_arrow.addEventListener('click', function(e) {
-    content[current].style.opacity = 0;
-    current--;
-    ArrowUpdate();
-    MountainUpdate();
-  })
+  left_arrow.addEventListener('click', function(e) { ChangePage(current - 1); });
 
-  right_arrow.addEventListener('click', function(e) {
-    content[current].style.opacity = 0;
-    if (right_arrow.classList.contains('fadedIn')) {
-      right_arrow.classList.remove('fadedIn');
-    }
-    current++;
-    ArrowUpdate(current);
-    MountainUpdate();
-  })
+  right_arrow.addEventListener('click', function(e) { ChangePage(current + 1); });
 
   $(document).bind('mousemove', function(e){
       $('#tail1').css({
@@ -120,47 +71,36 @@ function init(){
       });
   });
 
-  function ArrowUpdate() {
+  function ChangePage(pagenum){
+    // make old page invisible
+    content[current].style.opacity = 0;
+
+    current = pagenum;
+    console.log(current);
+
+    let topref;
+    let leftref;
+
+    // update visible arrows
     if (current == 0) {
       left_arrow.classList.remove("active");
-      menu.style.left = ''
+      right_arrow.classList.add("active");
     }
     else if (current == 4) {
+      left_arrow.classList.add("active");
       right_arrow.classList.remove("active");
     }
     else {
       left_arrow.classList.add("active");
       right_arrow.classList.add("active");
     }
-
-    if (left_arrow.classList.contains("active")) {
-      left_arrow.style.display = 'inline';
-    }
-    else {
-      left_arrow.style.display = 'none';
-    }
-
-    if (right_arrow.classList.contains("active")) {
-      right_arrow.style.display = 'inline';
-    }
-    else {
-      right_arrow.style.display = 'none';
-    }
-  }
-
-  function MountainUpdate() {
-    content[current].style.opacity = 1;
+    // scroll to page
     wrapper.scrollBy({
-      top: $(".content").eq(current).offset().top,
-      left: $(".content").eq(current).offset().left,
+      top: pages.eq(current).offset().top,
+      left: pages.eq(current).offset().left,
       behavior: 'smooth'
     })
+    // make new page visible
+    content[current].style.opacity = 1;
   }
-
 }
-
-/*
-if(window.pageYOffset != 0)
-  alert("You are at the top of this window");
-}
-*/
